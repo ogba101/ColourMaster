bd93141acab107696af27aa730175e54923a4afa
tobisitu <tobitisu@yahoo.co.uk>
2016-07-18 18:48:33 -0400

    committing activity user profile activity

---
+++ app/src/main/java/project/colormaster/activity_profile_manager.java
@@ -0,0 +1,17 @@
+package project.colormaster;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_profile_manager extends AppCompatActivity {
+    final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_manager);
+       }
+}
