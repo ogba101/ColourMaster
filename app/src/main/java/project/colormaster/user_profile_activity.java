21b1b6fc524fe811475414a0c7b54091e85132e6
OGBA <ogbarules@rocketmail.com>
2016-07-18 16:47:09 -0400

    Merge branch 'master' of https://github.com/tobisitu/ColourMaster
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ app/src/main/java/project/colormaster/user_profile_activity.java
@@ -0,0 +1,13 @@
+package project.colormaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_profile_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_activity);
+    }
+}
