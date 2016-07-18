959bd629818f77682240f5e02191778fa2180c1d
tobisitu <tobitisu@yahoo.co.uk>
2016-07-18 18:48:52 -0400

    committing activity user profile activity

---
+++ app/src/main/java/project/colormaster/activity_welcome.java
@@ -0,0 +1,30 @@
+package project.colormaster;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+
+
+
+public class activity_welcome extends AppCompatActivity {
+    final Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_activity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent i = new Intent(context,activity_profile_manager.class);
+                startActivity(i);
+            }
+        }, 1500);
+
+    }
+
+
+}
