31d8e240d5cf4b7ea8376b19a3421a6e4cd1e454
OGBA <ogbarules@rocketmail.com>
2016-07-19 18:00:54 -0400

    Merge branch 'master' of https://github.com/tobisitu/ColourMaster
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/drawable/profile_button.xml
    #	app/src/main/res/layout/activity_lesson__menu_.xml
    #	app/src/main/res/layout/activity_profile_manager.xml
    #	app/src/main/res/layout/activity_welcome_activity.xml
    #	app/src/main/res/layout/login_colormaster.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/project/colormaster/GlobalClass.java
@@ -0,0 +1,9 @@
+package project.colormaster;
+
+/**
+ * Created by OGBA on 16-07-19.
+ */
+public class GlobalClass {
+    int btnClicked; // the id of the button clicked
+    int scores[][]=new int[9][10];// 9 profiles 10 values
+}
