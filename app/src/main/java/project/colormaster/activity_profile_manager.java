3a926c018106e333b52df82e64d174faa667c940
tobisitu <tobitisu@yahoo.co.uk>
2016-07-18 20:12:18 -0400

    committing colours

--- app/src/main/java/project/colormaster/activity_profile_manager.java
+++ app/src/main/java/project/colormaster/activity_profile_manager.java
@@ -5,13 +5,28 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class activity_profile_manager extends AppCompatActivity {
+public class activity_profile_manager extends AppCompatActivity
+{
     final Context context = this;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_manager);
-       }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+       android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+    public void btn_add_profile (View v )
+    {
+        Intent intent = new Intent(context, login_activity.class);
+        startActivity(intent);
+    }
 }
